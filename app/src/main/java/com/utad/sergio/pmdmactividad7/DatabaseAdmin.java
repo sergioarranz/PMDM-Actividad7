581d713aef56dfdca5d130bc49b9487ce0488242
Sergio Arranz Sobrino <sergio.arranz@live.u-tad.com>
2018-02-27 00:31:31 +0100

    Add contacts DatabaseAdmin + implement

--- app/src/main/java/com/utad/sergio/pmdmactividad7/DatabaseAdmin.java
+++ app/src/main/java/com/utad/sergio/pmdmactividad7/DatabaseAdmin.java
@@ -1,6 +1,8 @@
 package com.utad.sergio.pmdmactividad7;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -46,5 +48,34 @@ public class DatabaseAdmin extends SQLiteOpenHelper {
 
         // Create tables again
         onCreate(db);
+    }
+
+    // Adding new contact
+    public void addContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_ID, contact.getID()); // Contact ID
+        values.put(KEY_NAME, contact.getName()); // Contact Name
+        values.put(KEY_PH_NO, contact.getPhoneNumber()); // Contact Phone Number
+
+        // Inserting Row
+        db.insert(TABLE_CONTACTS, null, values);
+        db.close(); // Closing database connection
+    }
+
+    public Contact getContact(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_CONTACTS, new String[] { KEY_ID,
+                        KEY_NAME, KEY_PH_NO }, KEY_ID + "=?",
+                new String[] { String.valueOf(id) }, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(2));
+        // return contact
+        return contact;
     }
 }
--- app/src/main/java/com/utad/sergio/pmdmactividad7/MainActivity.java
+++ app/src/main/java/com/utad/sergio/pmdmactividad7/MainActivity.java
@@ -2,12 +2,21 @@ package com.utad.sergio.pmdmactividad7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseAdmin databaseAdmin = new DatabaseAdmin(this);
+    Contact contact = new Contact(4,"Sergio", "3223233324");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseAdmin.addContact(contact);
+
+        Contact contact = databaseAdmin.getContact(3);
+        Log.v("Contact Data: ", contact.getName()+" --- "+contact.getPhoneNumber());
     }
 }
